0977dfee9e145ae0b30ccfd2bd55944e5a706c28
Reginaldo de Souza Santos <reginaldosantos.g10@gmail.com>
2026-06-15 15:13:00 -0300

    feat: substitui SVG do logo pela imagem logo.png no login, dashboard e catalogo

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = 'autolist-catalogo-v2';
+const CACHE_NAME = 'autolist-catalogo-v3';
 const ASSETS = [
   './',
   './index.html',
   './style.css',
   './app.js',
   './manifest.json',
+  './logo.png',
   './icon.png',
   './icon-192.png',
   './icon-512.png'
